2b4b98fdafc1a3909183db90da9f856cd62dc7f3
Neto test <anna.shuliko@bk.ru>
2023-04-30 19:28:46 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+    public static void main(String[] args) {
+      int initialBalance = 100;
+      int addend = 300;
+
+      int bonus = 0;
+      if (addend > 1000) {
+          bonus = addend / 100;
+      }
+
+      int finalBalance = initialBalance + addend + bonus;
+
+        System.out.println("Итоговый баланс: " + finalBalance);
+        System.out.println("Бонус: " + bonus);
+    }
 }
